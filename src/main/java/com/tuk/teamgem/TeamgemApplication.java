1b33cb6783cd8e28176dad01d4399ec108070795
songhaechan <bukak2019@naver.com>
2024-11-25 23:52:17 +0900

    initial commit

---
+++ src/main/java/com/tuk/teamgem/TeamgemApplication.java
@@ -0,0 +1,13 @@
+package com.tuk.teamgem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamgemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamgemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuk/teamgem/TeamgemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuk.teamgem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamgemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
